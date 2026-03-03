0de20d7749a81a1102ff5ccf3c4b410bae3ea5e4
ebbajoh <ebbajohansson999@gmail.com>
2026-03-03 15:27:12 +0100

    nytt

--- Projekt-ColorByNumber/main.m
+++ Projekt-ColorByNumber/main.m
@@ -16,7 +16,7 @@ title('Original (Smoothed)')
 % cluster into many colors
 
 %vi börjar med flera clusters
-K_initial = 30;   
+K = 10;   
 
 % hämtar storleken på bilden i cielab formatet
 [h,w,~] = size(lab);
@@ -24,40 +24,24 @@ K_initial = 30;
 %varje pixel blir en datapunkt i ett "3D färgrum"
 lab_reshaped = reshape(lab, [], 3);
 
-[idx, centers] = kmeans(lab_reshaped, K_initial);
+% --- Stable kmeans (improved) ---
+[idx, centers] = kmeans(lab_reshaped, K, ...
+    'Replicates', 5, ...
+    'MaxIter', 500, ...
+    'Start', 'plus');
 
-% Count cluster sizes
-counts = histcounts(idx, 1:K_initial+1);
-
-% Sort by dominance
-[~, sortedIdx] = sort(counts, 'descend');
-
-% Select 15 most dominant clusters
-K = 10;
-topK = sortedIdx(1:K);
-
-dominant_centers = centers(topK, :);
- 
-%forces every pixel in the image to become one of those dominant colors
-
-%Compute distance from every pixel to every dominant color
-D = pdist2(lab_reshaped, dominant_centers);
-
-%Find nearest dominant color
-[~, nearest] = min(D, [], 2);
-
-%Reshape into image format
-label_map = reshape(nearest, h, w);
+% reshape into image format
+label_map = reshape(idx, h, w);
 
 % Reconstruct quantized image
-lab_quant = dominant_centers(nearest, :);
+lab_quant = centers(idx, :);
 lab_quant = reshape(lab_quant, h, w, 3);
 
 rgb_quant = lab2rgb(lab_quant);
 
 figure
 imshow(rgb_quant)
-title('15 Most Dominant Colors')
+title('10 Dominant Colors')
 
 
 % remove small regions
@@ -81,7 +65,7 @@ valid_mask = clean_map > 0;
 clean_map(~valid_mask) = clean_map(idx_nearest(~valid_mask));
 
 % Reconstruct cleaned image
-lab_clean = dominant_centers(clean_map(:), :);
+lab_clean = centers(clean_map(:), :);
 lab_clean = reshape(lab_clean, h, w, 3);
 
 rgb_clean = lab2rgb(lab_clean);
@@ -91,7 +75,7 @@ imshow(rgb_clean)
 title('After Removing Small Regions')
 
 
-% Hittar gränser mellan färgområdenfff
+% Hittar gränser mellan färgområden
 
 % Detect boundaries via label difference
 boundaries = false(h, w);
@@ -102,7 +86,10 @@ boundaries(1:end-1,:) = boundaries(1:end-1,:) | ...
                         (clean_map(1:end-1,:) ~= clean_map(2:end,:));
 
 % Thin to single-pixel lines
-boundaries = bwmorph(boundaries, 'thin', 2);
+boundaries = bwmorph(boundaries, 'thin', 1);
+
+% Make lines slightly thicker (better for printing)
+boundaries = imdilate(boundaries, strel('disk',1));
 
 outline = ones(h, w);
 outline(boundaries) = 0;
